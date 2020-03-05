9a4ed45228dd31b7e1e11d1de8034d73e78fab96
Marvin9 <marvinduff97@gmail.com>
2020-03-05 17:26:11 +0530

    add connectionURI

--- tests/adapter.test.js
+++ tests/adapter.test.js
@@ -1,14 +1,10 @@
 const PostgreAdapter = require('../lib/adapter');
 
-const connectionConfig = {
-  user: 'postgres',
-  password: ' ',
-  database: 'temp',
-};
+const connectionURI = 'postgresql://postgres: @localhost:5432/temp';
 
 test('adapter should properly connected to database', async () => {
   try {
-    const client = new PostgreAdapter(connectionConfig);
+    const client = new PostgreAdapter(connectionURI);
     await client.open();
     await client.close();
   } catch (err) {
